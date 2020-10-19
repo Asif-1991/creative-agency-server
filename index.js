e88e3347f49b6b73d2408bcfa3e610e9b2fc4859
Asif-91 <asifchowdhury323@gmail.com>
2020-10-19 22:09:38 +0600

    ready for connect

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 
 const port = 5000;
-
 const app = express()
 
 app.use(cors());
@@ -20,12 +19,74 @@ app.get('/', (req, res)=>{
 
 })
 
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("creativeAgency").collection("services");
+  const orderCollection = client.db("creativeAgency").collection("orders");
+  const reviewCollection = client.db("creativeAgency").collection("reviews");
 
-    
+  const servicesCollection = client.db("creativeAgency").collection("services");
+  const adminCollection = client.db("creativeAgency").collection("admin");
+
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    orderCollection.insertOne(order)
+    .then((result) => {
+        res.send(result)
+    })
+  })
+
+  app.get('/allOrder', (req, res) => {
+    orderCollection.find({})
+    .toArray((err, documents) => {
+        res.send(documents)
+    })
+  })
+
+  app.post('/addReview', (req, res) => {
+    const review = req.body;
+    reviewCollection.insertOne(review)
+    .then((result) => {
+        res.send(result)
+    })
+  })
+
+  app.get('/review', (req, res) => {
+    reviewCollection.find({})
+    .toArray((err, documents) => {
+        res.send(documents)
+    })
+  })
+
+  app.post('/addService', (req, res) => {
+    const service = req.body;
+    serviceCollection.insertOne(service)
+    .then((result) => {
+        res.send(result)
+    })
+  })
+
+  app.get('/services', (req, res) => {
+    servicesCollection.find({})
+    .toArray((err, documents) => {
+        res.send(documents)
+    })
+  })
+
+  app.post('/adminEmail', (req, res) => {
+    const email = req.body;
+    adminCollection.insertOne(email)
+    .then((result) => {
+        res.send(result)
+    })
+  })
+
+  app.get('/isAdmin', (req, res) => {
+    const email = req.body.email;
+    adminCollection.find({ email: email })
+    .toArray((err, adminEmail) => {
+        res.send(adminEmail)
+    })
+  })
 
 });
 
